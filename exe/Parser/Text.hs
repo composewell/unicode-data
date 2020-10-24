bacd4ce22b2aea237f0ffcbb2ee6fc0e2216acc8
adithyaov <adithya@composewell.com>
2020-10-24 17:27:31 +0530

    Merge the Common module into Text.Parser

--- exe/Parser/Text.hs
+++ exe/Parser/Text.hs
@@ -17,7 +17,9 @@
 
 module Parser.Text where
 
-import Data.Char (ord, isSpace)
+import Data.Bits (shiftL)
+import Data.Char (chr, ord, isSpace)
+import Data.List (unfoldr)
 import Data.Maybe (fromMaybe)
 import Data.Function ((&))
 import Data.Unicode.Properties.DecomposeHangul (isHangul)
@@ -31,8 +33,34 @@ import qualified Streamly.Internal.FileSystem.Handle as H
 import qualified Streamly.Internal.Data.Unicode.Stream as U
 import qualified System.IO as Sys
 
-import Common
 import Prelude hiding (pred)
+
+--------------------------------------------------------------------------------
+-- Types
+--------------------------------------------------------------------------------
+
+data GeneralCategory =
+    Lu|Ll|Lt|             --LC
+    Lm|Lo|                --L
+    Mn|Mc|Me|             --M
+    Nd|Nl|No|             --N
+    Pc|Pd|Ps|Pe|Pi|Pf|Po| --P
+    Sm|Sc|Sk|So|          --S
+    Zs|Zl|Zp|             --Z
+    Cc|Cf|Cs|Co|Cn        --C
+    deriving (Show, Read)
+
+data DecompType =
+       DTCanonical | DTCompat  | DTFont
+     | DTNoBreak   | DTInitial | DTMedial   | DTFinal
+     | DTIsolated  | DTCircle  | DTSuper    | DTSub
+     | DTVertical  | DTWide    | DTNarrow
+     | DTSmall     | DTSquare  | DTFraction
+    deriving (Show, Eq)
+
+data Decomp = DCSelf | DC [Char] deriving (Show, Eq)
+
+data DType = Canonical | Kompat
 
 data DetailedChar =
     DetailedChar
@@ -45,7 +73,59 @@ data DetailedChar =
         }
     deriving (Show)
 
--- XXX We seem to be adding a few additional cases for Canonical
+--------------------------------------------------------------------------------
+-- Helpers
+--------------------------------------------------------------------------------
+
+readCodePoint :: String -> Char
+readCodePoint = chr . read . ("0x"++)
+
+genSignature :: String -> String
+genSignature testBit = testBit <> " :: Char -> Bool"
+
+-- | Check that var is between minimum and maximum of orderList
+genRangeCheck :: String -> [Int] -> String
+genRangeCheck var ordList =
+      var <> " >= "
+      <> show (minimum ordList) <> " && " <> var <> " <= "
+      <> show (maximum ordList)
+
+genBitmap :: String -> [Int] -> String
+genBitmap funcName ordList = unlines
+  [ "{-# INLINE " ++ funcName  ++ " #-}"
+  , genSignature funcName
+  , funcName <> " = \\c -> let n = ord c in " ++ genRangeCheck "n" ordList ++ " && lookupBit64 bitmap# n"
+  , "  where"
+  , "    bitmap# = " ++ show (bitMapToAddrLiteral (positionsToBitMap ordList)) ++ "#"
+  ]
+
+positionsToBitMap :: [Int] -> [Bool]
+positionsToBitMap = go 0
+  where
+    go _ [] = []
+    go i xxs@(x : xs)
+      | i < x     = False : go (i + 1) xxs
+      | otherwise = True  : go (i + 1) xs
+
+bitMapToAddrLiteral :: [Bool] -> String
+bitMapToAddrLiteral = map (chr . toByte . padTo8) . unfoldr go
+  where
+    go :: [a] -> Maybe ([a], [a])
+    go [] = Nothing
+    go xs = Just (take 8 xs, drop 8 xs)
+
+    padTo8 :: [Bool] -> [Bool]
+    padTo8 xs
+      | length xs >= 8 = xs
+      | otherwise = xs ++ replicate (8 - length xs) False
+
+    toByte :: [Bool] -> Int
+    toByte xs = sum $ map (\i -> if xs !! i then 1 `shiftL` i else 0) [0..7]
+
+--------------------------------------------------------------------------------
+-- Parser
+--------------------------------------------------------------------------------
+
 readDecomp :: String -> (Maybe DecompType, Decomp)
 readDecomp s =
     if length wrds == 0
@@ -361,7 +441,6 @@ genCorePropertiesModule file props =
         , "import Data.Char (ord)"
         , "import Data.Unicode.Internal.Bits (lookupBit64)"
         ]
-
 
 parseDetailedChar :: String -> DetailedChar
 parseDetailedChar line =
