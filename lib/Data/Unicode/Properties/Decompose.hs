1bc7a3a029de27c1b4fc69055dce047e390590c1
adithyaov <adithya@composewell.com>
2020-11-18 08:59:30 +0530

    Add simple documentation to a few exposed functions

--- lib/Data/Unicode/Properties/Decompose.hs
+++ lib/Data/Unicode/Properties/Decompose.hs
@@ -25,11 +25,15 @@ import qualified Data.Unicode.Properties.DecompositionsK as K
 
 data DecomposeMode = DecomposeNFD | DecomposeNFKD
 
+-- | Given the 'DecomposeMode' @D@ and a character @c@, decompose @c@ into its
+-- normal form in @D@.
 {-# INLINE decomposeChar #-}
 decomposeChar :: DecomposeMode -> Char -> [Char]
 decomposeChar DecomposeNFD  = D.decomposeChar
 decomposeChar DecomposeNFKD = K.decomposeChar
 
+-- | Given the 'DecomposeMode' @D@ and a character @c@, return True if @c@ is
+-- decomposable in @D@. This does not work for Hangul characters.
 {-# INLINE isDecomposable #-}
 isDecomposable :: DecomposeMode -> Char -> Bool
 isDecomposable DecomposeNFD  = D.isDecomposable
