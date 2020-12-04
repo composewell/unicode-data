fe6756ec99734f753a450cd5a80bea7914545f5d
adithyaov <adithya@composewell.com>
2020-12-04 21:45:39 +0530

    Add Unicode.Properties.Core

---
+++ lib/Unicode/Properties/Core.hs
@@ -0,0 +1,33 @@
+-- |
+-- Module      : Unicode.Properties.Core
+-- Copyright   : (c) 2020 Composewell Technologies and Contributors
+-- License     : Apache-2.0
+-- Maintainer  : streamly@composewell.com
+-- Stability   : experimental
+--
+module Unicode.Properties.Core
+    ( isAlpha
+    , isUpper
+    , isLower
+    )
+where
+
+import qualified Unicode.Internal.Generated.DerivedCoreProperties as P
+
+-- | Selects Alphabetic Unicode characters defined in Unicode character
+-- database.  This should be equivalent to "Data.Char.isAlpha".
+{-# INLINE isAlpha #-}
+isAlpha :: Char -> Bool
+isAlpha = P.isAlphabetic
+
+-- | Selects Uppercase Unicode characters defined in Unicode character database.
+-- This should be equivalent to "Data.Char.isUpper".
+{-# INLINE isUpper #-}
+isUpper :: Char -> Bool
+isUpper = P.isUppercase
+
+-- | Selects Lowercase Unicode characters defined in Unicode character database.
+-- This should be equivalent to "Data.Char.isLower".
+{-# INLINE isLower #-}
+isLower :: Char -> Bool
+isLower = P.isLowercase
