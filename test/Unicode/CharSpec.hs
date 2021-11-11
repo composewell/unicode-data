63efcf6275291f0e6a769711d2fd8142dc4166c8
Pierre Le Marre <dev@wismill.eu>
2021-11-11 23:32:20 +0100

    Disable tests for GHC with incompatible Unicode versions.

--- test/Unicode/CharSpec.hs
+++ test/Unicode/CharSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE CPP #-}
 
 module Unicode.CharSpec
   ( spec
@@ -13,17 +14,34 @@ import Test.Hspec
 These tests may fail if the compiler’s Unicode version
 does not match the version of this package.
 
-* GHC 8.10 = Unicode 13.0
++-------------+----------------+-----------------+
+| GHC version | @base@ version | Unicode version |
++=============+================+=================+
+| 8.8         | 4.13           | 12.0            |
+| 8.10.[1-4]  | 4.14.{0,1}     | 12.0            |
+| 8.10.5+     | 4.14.2+        | 13.0            |
+| 9.0.1       | 4.15.0         | 12.1            |
+| 9.2.1       | 4.16.0         | 14.0            |
++-------------+----------------+-----------------+
 -}
 
 spec :: Spec
 spec = do
+#ifdef COMPATIBLE_GHC_UNICODE
   describe "Unicode general categories" do
+#else
+  let xdescribe' t = before_ (pendingWith "Incompatible GHC Unicode version") . describe t
+  xdescribe' "Unicode general categories" do
+#endif
     it "generalCategory" do
       -- Note: we cannot compare the categories directly, so use 'show'.
       let sameGC c = show (UChar.generalCategory c) == show (Char.generalCategory c)
       traverse_ (`shouldSatisfy` sameGC) [minBound..maxBound]
+#ifdef COMPATIBLE_GHC_UNICODE
   describe "Character classification" do
+#else
+  xdescribe' "Character classification" do
+#endif
     it "isAlpha" do
       let same c = UChar.isAlpha c == Char.isAlpha c
       traverse_ (`shouldSatisfy` same) [minBound..maxBound]
@@ -56,4 +74,16 @@ spec = do
       traverse_ (`shouldSatisfy` same) [minBound..maxBound]
     it "isSymbol" do
       let same c = UChar.isSymbol c == Char.isSymbol c
+      traverse_ (`shouldSatisfy` same) [minBound..maxBound]
+#ifdef COMPATIBLE_GHC_UNICODE
+  describe "Case" do
+#else
+  xdescribe' "Case" do
+#endif
+    let xit' t = before_ (pendingWith "Incompatible implementation with Data.Char") . it t
+    xit' "isLower" do
+      let same c = UChar.isLower c == Char.isLower c
+      traverse_ (`shouldSatisfy` same) [minBound..maxBound]
+    xit' "isUpper" do
+      let same c = UChar.isUpper c == Char.isUpper c
       traverse_ (`shouldSatisfy` same) [minBound..maxBound]
