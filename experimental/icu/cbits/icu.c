08ffbbf4f3e2a53ad010617730b7c9722c7cebc7
Pierre Le Marre <dev@wismill.eu>
2024-06-15 11:15:37 +0200

    icu: Add scripts support

--- experimental/icu/cbits/icu.c
+++ experimental/icu/cbits/icu.c
@@ -1,10 +1,15 @@
 #include <unicode/utypes.h>
 #include <unicode/uchar.h>
+#include <unicode/uscript.h>
 #include "icu.h"
 
 void __hs_u_getUnicodeVersion(UVersionInfo versionArray) {
     u_getUnicodeVersion(versionArray);
 }
+
+/*******************************************************************************
+ * Names
+ ******************************************************************************/
 
 int32_t __hs_u_charName( UChar32 codepoint
                        , UCharNameChoice nameChoice
@@ -20,4 +25,25 @@ void __hs_u_charAge( UChar32 c, UVersionInfo versionArray ) {
 
 int8_t __hs_u_charType(UChar32 c) {
     return u_charType(c);
+}
+
+/*******************************************************************************
+ * Scripts
+ ******************************************************************************/
+
+UScriptCode __hs_uscript_getScript(UChar32 codepoint) {
+    UErrorCode err = U_ZERO_ERROR;
+    return uscript_getScript(codepoint, &err);
+}
+
+int32_t __hs_uscript_getScriptExtensions
+    ( UChar32 codepoint
+    , UScriptCode * scripts
+    , int32_t capacity ) {
+    UErrorCode err = U_ZERO_ERROR;
+    return uscript_getScriptExtensions(codepoint, scripts, capacity, &err);
+}
+
+const char * __hs_uscript_getShortName(UScriptCode scriptCode) {
+    return uscript_getShortName(scriptCode);
 }
